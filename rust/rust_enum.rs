9692ecfaaf0f5ff0eb1a371592628794257e53f0
sjb <idor@vip.qq.com>
2023-01-17 15:40:17 +0800

    temp

--- rust/rust_enum.rs
+++ rust/rust_enum.rs
@@ -1,5 +1,3 @@
-// https://www.codingame.com/ide/demo/972988e4ddd83a948a90b8a9006932fb158b18
-use std::convert::TryInto;
 use std::io;
 use std::fmt::{Debug, Display, Formatter, Result};
 
@@ -7,9 +5,9 @@ use std::fmt::{Debug, Display, Formatter, Result};
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum LogLevel {
-    FATAL = 0, ERROR = 1, WARNING = 2, INFO = 3, DEBUG = 4
+    FATAL, ERROR, WARNING, INFO, DEBUG, UNKNOW
 }
 
 impl Display for LogLevel {
@@ -19,19 +17,22 @@ impl Display for LogLevel {
             LogLevel::ERROR => write!(f, "ERROR"),
             LogLevel::WARNING => write!(f, "WARING"),
             LogLevel::INFO => write!(f, "WARING"),
-            LogLevel::DEBUG => write!(f, "DEBUG")
+            LogLevel::DEBUG => write!(f, "DEBUG"),
+            _ => write!(f, "UNKNOW")
         }
     }
 }
 
 impl From<LogLevel> for String {
     fn from(level: LogLevel) -> Self{
+        println!("FATAL impl form");
         match level {
             LogLevel::FATAL => "FATAL".into(),
             LogLevel::ERROR => "ERROR".into(),
             LogLevel::WARNING => "WARNING".into(),
             LogLevel::INFO => "INFO".into(),
             LogLevel::DEBUG => "DEBUG".into(),
+            LogLevel::UNKNOW => "UNKNOW".into()
         }
     }
 }
@@ -39,12 +40,23 @@ impl From<LogLevel> for String {
 impl From<String> for LogLevel {
     fn from(level: String) -> Self {
         match level {
-            level if level == "FATAL" => LogLevel::FATAL,
-            level if level == "ERROR" || level == "ERR"  => LogLevel::ERROR,
-            level if level == "WARNING" || level =="WARN" => LogLevel::WARNING,
-            level if level == "INFO" => LogLevel::INFO,
-            level if level == "DEBUG" => LogLevel::DEBUG,
-            _ => LogLevel::DEBUG,
+            // 为什么这里不能比较？
+            level if level == LogLevel::FATAL.to_string() => {
+                println!("FATAL for LogLevel : {}", level);
+                LogLevel::FATAL
+            },
+            level if level == LogLevel::ERROR.to_string() || level == "ERR"  => LogLevel::ERROR,
+            level if level == LogLevel::WARNING.to_string() || level =="WARN" => LogLevel::WARNING,
+            level if level == LogLevel::INFO.to_string() => LogLevel::INFO,
+            level if level == LogLevel::DEBUG.to_string() => {
+                println!("DEBUG for LogLevel : {}", level);
+                LogLevel::DEBUG
+            },
+            _ => {
+                // 为什么用 level 的值是FATAL会到这里来？
+                println!("Default for LogLevel : {}", level);
+                LogLevel::UNKNOW
+            }
         }
     }
 }
@@ -58,19 +70,26 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let lvl = parse_input!(input_line, i32);
+    // println!("lvl: {}", lvl);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
-    for i in 0..n as usize {
+    for _i in (0..n as usize) {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_matches('\n').to_string();
+        // println!("line: {}", line);
         let mut split = line.split('>');
-        let mut condition = split.next(); // FATAL: could not
-        condition.map( x => x.)
-        let mut contains_comma 
-        let level: LogLevel = line.to_string().into();
+        split.next();
+        let condition = split.next().unwrap(); // FATAL: could not
+        // println!("condition: {}", condition);
+        let mut split = condition.split(':');
+        let level_str = split.next().unwrap();
+        println!("level charactor: {}", level_str);
+        let level: LogLevel = level_str.to_string().into();
+        println!("level: {}", level);
         let level_number = level as i32;
+        println!("LEVEL:{}, level: {}, lvl: {}, level <= lvl: {}", level, level_number, lvl, level_number <= lvl);
         if level_number <= lvl {
             println!("{}", line);
         }
@@ -80,3 +99,8 @@ fn main() {
     // To debug: eprintln!("Debug message...");
     
 }
+
+// REFERENCES:
+// https://www.codingame.com/ide/demo/972988e4ddd83a948a90b8a9006932fb158b18
+// https://zhuanlan.zhihu.com/p/454311400
+// https://blog.csdn.net/eryk86/article/details/122958621
