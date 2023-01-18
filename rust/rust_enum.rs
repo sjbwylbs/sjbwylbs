bef9cb3c1f15ad0a11255b342f83b7b123205879
sjb <idor@vip.qq.com>
2023-01-18 17:40:08 +0800

    passed

--- rust/rust_enum.rs
+++ rust/rust_enum.rs
@@ -15,8 +15,8 @@ impl Display for LogLevel {
         match self {
             LogLevel::FATAL => write!(f, "FATAL"),
             LogLevel::ERROR => write!(f, "ERROR"),
-            LogLevel::WARNING => write!(f, "WARING"),
-            LogLevel::INFO => write!(f, "WARING"),
+            LogLevel::WARNING => write!(f, "WARNING"),
+            LogLevel::INFO => write!(f, "INFO"),
             LogLevel::DEBUG => write!(f, "DEBUG"),
             _ => write!(f, "UNKNOW")
         }
@@ -25,7 +25,6 @@ impl Display for LogLevel {
 
 impl From<LogLevel> for String {
     fn from(level: LogLevel) -> Self{
-        println!("FATAL impl form");
         match level {
             LogLevel::FATAL => "FATAL".into(),
             LogLevel::ERROR => "ERROR".into(),
@@ -39,24 +38,14 @@ impl From<LogLevel> for String {
 
 impl From<String> for LogLevel {
     fn from(level: String) -> Self {
+        // println!("{}", level);
         match level {
-            // 为什么这里不能比较？
-            level if level == LogLevel::FATAL.to_string() => {
-                println!("FATAL for LogLevel : {}", level);
-                LogLevel::FATAL
-            },
+            level if level == LogLevel::FATAL.to_string() || level == "FAT" => LogLevel::FATAL,
             level if level == LogLevel::ERROR.to_string() || level == "ERR"  => LogLevel::ERROR,
-            level if level == LogLevel::WARNING.to_string() || level =="WARN" => LogLevel::WARNING,
-            level if level == LogLevel::INFO.to_string() => LogLevel::INFO,
-            level if level == LogLevel::DEBUG.to_string() => {
-                println!("DEBUG for LogLevel : {}", level);
-                LogLevel::DEBUG
-            },
-            _ => {
-                // 为什么用 level 的值是FATAL会到这里来？
-                println!("Default for LogLevel : {}", level);
-                LogLevel::UNKNOW
-            }
+            level if level == LogLevel::WARNING.to_string() || level =="WAR" => LogLevel::WARNING,
+            level if level == LogLevel::INFO.to_string() || level == "INF" => LogLevel::INFO,
+            level if level == LogLevel::DEBUG.to_string() || level == "DEB"=> LogLevel::DEBUG,
+            _ => LogLevel::UNKNOW
         }
     }
 }
@@ -70,27 +59,30 @@ fn main() {
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let lvl = parse_input!(input_line, i32);
-    // println!("lvl: {}", lvl);
+    // eprintln!("lvl: {}", lvl);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
-    for _i in (0..n as usize) {
+    for _i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_matches('\n').to_string();
-        // println!("line: {}", line);
+        eprintln!("line: {}", line);
         let mut split = line.split('>');
-        split.next();
-        let condition = split.next().unwrap(); // FATAL: could not
-        // println!("condition: {}", condition);
+        let start  = split.next();
+        let condition = split.next().unwrap(); 
+        eprintln!("condition: {}", condition);
         let mut split = condition.split(':');
-        let level_str = split.next().unwrap();
-        println!("level charactor: {}", level_str);
-        let level: LogLevel = level_str.to_string().into();
-        println!("level: {}", level);
+        let level_str = split.next().unwrap().trim();
+        let level_uppercase = level_str.to_uppercase();
+        // eprintln!("level charactor: {}", level_str);
+        let level: LogLevel = level_uppercase.to_string().into();
+        // eprintln!("level: {}, {}", level, level_str);
         let level_number = level as i32;
-        println!("LEVEL:{}, level: {}, lvl: {}, level <= lvl: {}", level, level_number, lvl, level_number <= lvl);
+        // eprintln!("LEVEL:{}, level str {}, level: {}, lvl: {}, level <= lvl: {}", level, level_str, level_number, lvl, level_number <= lvl);
+
         if level_number <= lvl {
+            let line = start.unwrap().to_string() + ">" + condition.replacen(level_str, level.to_string().as_str(), 1).as_str();
             println!("{}", line);
         }
     }
