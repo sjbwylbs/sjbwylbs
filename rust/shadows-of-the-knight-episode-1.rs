f2931a18349a60a5b0d51c2b9ef0ead2510fc1b2
sjb <idor@vip.qq.com>
2023-02-11 14:42:01 +0800

    add readable code

--- rust/shadows-of-the-knight-episode-1.rs
+++ rust/shadows-of-the-knight-episode-1.rs
@@ -1,40 +1,180 @@
-fn read_input<T: std::str::FromStr>() -> T {
-    let mut input_line = String::new();
-    std::io::stdin().read_line(&mut input_line).unwrap();
-    input_line.trim_matches('\n').parse().ok().unwrap()
-}
+use std::io::{stdin, BufRead};
 
-fn read_inputs<T: std::str::FromStr>() -> Vec<T> {
-    read_input::<String>().split_whitespace().map(|s| s.parse().ok().unwrap()).collect()
-}
+#[derive(Debug, Clone, Copy)]
+struct Pair(i32, i32);
 
-fn main() {
-    let building = read_inputs::<i32>();
-    let mut xmax = building[0]; // width of the building.
-    let mut ymax = building[1]; // height of the building.
-    let _n: i32 = read_input(); // maximum number of turns before game over.
-    let start = read_inputs::<i32>();
-    let mut x0 = start[0];
-    let mut y0 = start[1];
-
-    let mut xmin = 0;
-    let mut ymin = 0;
-
-    loop {
-        let bomb_dir: String = read_input(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-
-        if !bomb_dir.contains("D") { ymax = y0 }
-        if !bomb_dir.contains("U") { ymin = y0 + 1 }
-        if !bomb_dir.contains("R") { xmax = x0 }
-        if !bomb_dir.contains("L") { xmin = x0 + 1 }
-
-        x0 = (xmin + xmax) / 2;
-        y0 = (ymin + ymax) / 2;
-
-        // the location of the next window Batman should jump to.
-        println!("{} {}", x0, y0);
+impl std::str::FromStr for Pair {
+    type Err = Box<dyn std::error::Error + Send>;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s
+            .trim()
+            .split_whitespace()
+            .map(|i| i.parse().unwrap())
+            .collect();
+        Ok(Self(parts[0], parts[1]))
     }
 }
+
+type Building = Pair;
+type Batman = Pair;
+
+impl std::fmt::Display for Batman {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.0, self.1)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PossibleBombLocs {
+    x_start: i32,
+    y_start: i32,
+    x_end: i32,
+    y_end: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum BombDir {
+    Up,
+    UpRight,
+    Right,
+    DownRight,
+    Down,
+    DownLeft,
+    Left,
+    UpLeft,
+}
+
+impl std::str::FromStr for BombDir {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "U" => Ok(Self::Up),
+            "UR" => Ok(Self::UpRight),
+            "R" => Ok(Self::Right),
+            "DR" => Ok(Self::DownRight),
+            "D" => Ok(Self::Down),
+            "DL" => Ok(Self::DownLeft),
+            "L" => Ok(Self::Left),
+            "UL" => Ok(Self::UpLeft),
+            _ => Err("Invalid Direction"),
+        }
+    }
+}
+
+pub fn main() {
+    let stdin = stdin();
+    let mut lines = stdin.lock().lines().map(Result::unwrap);
+    let mut read_line = || lines.next().unwrap();
+    let building = read_line().parse::<Building>().unwrap();
+    let mut jumps = read_line().parse::<i32>().unwrap();
+    let mut batman = read_line().parse::<Batman>().unwrap();
+
+    let mut bomb_loc = PossibleBombLocs {
+        x_start: 0,
+        y_start: 0,
+        x_end: building.0,
+        y_end: building.1,
+    };
+
+    while jumps > 0 {
+        let direction = read_line().parse::<BombDir>().unwrap();
+        update_possible_bomb_locs(&mut bomb_loc, direction, batman);
+        set_new_batman_pos(&mut batman, &bomb_loc);
+        println!("{}", batman);
+        if bomb_loc.x_end - bomb_loc.x_start <= 1 && bomb_loc.y_end - bomb_loc.y_start <= 1 {
+            break;
+        }
+        jumps -= 1;
+    }
+}
+
+fn set_new_batman_pos(batman: &mut Batman, bomb_loc: &PossibleBombLocs) {
+    let new_x = (bomb_loc.x_start + bomb_loc.x_end) / 2;
+    let new_y = (bomb_loc.y_start + bomb_loc.y_end) / 2;
+    batman.0 = new_x;
+    batman.1 = new_y;
+}
+
+fn update_possible_bomb_locs(bomb_loc: &mut PossibleBombLocs, direction: BombDir, batman: Batman) {
+    match direction {
+        BombDir::Up => {
+            bomb_loc.x_start = batman.0;
+            bomb_loc.x_end = batman.0 + 1;
+            bomb_loc.y_end = batman.1;
+        }
+        BombDir::UpRight => {
+            bomb_loc.x_start = batman.0 + 1;
+            bomb_loc.y_end = batman.1;
+        }
+        BombDir::Right => {
+            bomb_loc.x_start = batman.0 + 1;
+            bomb_loc.y_start = batman.1;
+            bomb_loc.y_end = batman.1 + 1;
+        }
+        BombDir::DownRight => {
+            bomb_loc.x_start = batman.0 + 1;
+            bomb_loc.y_start = batman.1 + 1;
+        }
+        BombDir::Down => {
+            bomb_loc.x_start = batman.0;
+            bomb_loc.x_end = batman.0 + 1;
+            bomb_loc.y_start = batman.1 + 1;
+        }
+        BombDir::DownLeft => {
+            bomb_loc.x_end = batman.0;
+            bomb_loc.y_start = batman.1 + 1;
+        }
+        BombDir::Left => {
+            bomb_loc.x_end = batman.0;
+            bomb_loc.y_start = batman.1;
+            bomb_loc.y_end = batman.1 + 1;
+        }
+        BombDir::UpLeft => {
+            bomb_loc.x_end = batman.0;
+            bomb_loc.y_end = batman.1;
+        }
+    }
+}
+
+
+// fn read_input<T: std::str::FromStr>() -> T {
+//     let mut input_line = String::new();
+//     std::io::stdin().read_line(&mut input_line).unwrap();
+//     input_line.trim_matches('\n').parse().ok().unwrap()
+// }
+
+// fn read_inputs<T: std::str::FromStr>() -> Vec<T> {
+//     read_input::<String>().split_whitespace().map(|s| s.parse().ok().unwrap()).collect()
+// }
+
+// fn main() {
+//     let building = read_inputs::<i32>();
+//     let mut xmax = building[0]; // width of the building.
+//     let mut ymax = building[1]; // height of the building.
+//     let _n: i32 = read_input(); // maximum number of turns before game over.
+//     let start = read_inputs::<i32>();
+//     let mut x0 = start[0];
+//     let mut y0 = start[1];
+
+//     let mut xmin = 0;
+//     let mut ymin = 0;
+
+//     loop {
+//         let bomb_dir: String = read_input(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+//         if !bomb_dir.contains("D") { ymax = y0 }
+//         if !bomb_dir.contains("U") { ymin = y0 + 1 }
+//         if !bomb_dir.contains("R") { xmax = x0 }
+//         if !bomb_dir.contains("L") { xmin = x0 + 1 }
+
+//         x0 = (xmin + xmax) / 2;
+//         y0 = (ymin + ymax) / 2;
+
+//         // the location of the next window Batman should jump to.
+//         println!("{} {}", x0, y0);
+//     }
+// }
 
 // use std::io;
 
